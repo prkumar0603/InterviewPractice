35b7a2d33de64f61fd1b9266bcf0cbfe10935513
Pranav <34658584+prkumar0603@users.noreply.github.com>
2021-01-30 18:09:31 +0530

    subArray with maximum sum (imp)

--- Array/kadaneAlgorithm.java
+++ Array/kadaneAlgorithm.java
@@ -4,18 +4,28 @@ public class kadaneAlgorithm {
     public static void main(String[] args) {
        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
 
+       int start = 0;   // for the start index of subarray
+       int end = 0;     // for the end index of subarray
+       int s =0;
+
        int max_sum = Integer.MIN_VALUE;
        int curr_sum = 0;
 
        for (int i=0; i<arr.length;i++){
            curr_sum += arr[i];
 
-           if (curr_sum > max_sum)
-               max_sum = curr_sum;
-
-           if (curr_sum < 0)
+           if (curr_sum < 0){
                curr_sum = 0;
+               s = i+1;
+           }
+           else if (curr_sum > max_sum){
+               max_sum = curr_sum;
+               end =i;
+
+               start = s;
+           }
        }
+        System.out.println("start="+ start+"  end="+end);
         System.out.println("Maximum sum = "+max_sum);
     }
 }
