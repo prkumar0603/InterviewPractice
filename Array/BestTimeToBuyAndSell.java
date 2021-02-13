ec9f59403a38c6568425ad8ccf282e9285438b89
Pranav <34658584+prkumar0603@users.noreply.github.com>
2021-02-13 18:26:18 +0530

    best time to buy and sell

---
+++ Array/BestTimeToBuyAndSell.java
@@ -0,0 +1,41 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+Constraints:
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+*/
+
+public class BestTimeToBuyAndSell {
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        int max_profit = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i=0;i<prices.length;i++){
+            if (prices[i] < min){
+                min = prices[i];
+            }
+            else if(prices[i]-min < max_profit){
+                max_profit = prices[i] - min;
+            }
+        }
+
+        System.out.println("max = "+ max_profit);
+
+    }
+}
+/*Approach - traverse thru the array. two condition possible -
+1) current price < minimum
+2) find difference between current element and minimum is < max_profit.
+ */
