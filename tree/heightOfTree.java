b8ae8a850c2b34c9d2dcafee2daebef7e1d4b547
Pranav <34658584+prkumar0603@users.noreply.github.com>
2021-04-05 00:38:52 +0530

    height of a binary tree

---
+++ tree/heightOfTree.java
@@ -0,0 +1,32 @@
+package tree;
+
+public class heightOfTree {
+    public static void main(String[] args) {
+        demoNode root = new demoNode(3);
+        root.left = new demoNode(9);
+        root.right = new demoNode(20);
+        root.left.left = new demoNode(32);
+        root.left.right = new demoNode(3);
+        root.right.left = new demoNode(15);
+        root.right.right = new demoNode(7);
+
+        int res = treeHeight(root);
+        System.out.println(res);
+    }
+
+    private static int treeHeight(demoNode root) {
+        // base
+        if (root == null)
+            return 0;
+
+        // hypothesis
+        int leftHeight = treeHeight(root.left);
+        int rightheight = treeHeight(root.right);
+
+        // induction
+        int height = 1 + Math.max(leftHeight, rightheight);
+            // 1 is added to include that node
+
+        return height;
+    }
+}
