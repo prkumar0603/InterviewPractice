f15a305e40cd76e7f23cb8806f0fc6aee00fdfee
Pranav <34658584+prkumar0603@users.noreply.github.com>
2021-04-04 21:06:02 +0530

    reverse level order traversal of btree

--- tree/reverseLevelOrder.java
+++ tree/reverseLevelOrder.java
@@ -5,7 +5,20 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-/* In reverse level order, the lst level is printed first.
+/*
+Given a binary tree, returns of all its levels in a bottom-up fashion (i.e. last level towards the
+root). Ex: Given the following tree…
+        2
+       / \
+      1   2
+return [[1, 2], [2]]
+Ex: Given the following tree…
+       7
+      / \
+    6    2
+   / \
+  3   3
+return [[3, 3], [6, 2], [7]]
 */
 public class reverseLevelOrder {
     public static void main(String[] args) {
@@ -44,3 +57,6 @@ public class reverseLevelOrder {
         return result;
     }
 }
+/* we can also do this using a queue and stack. we add elements in stack by popping element from queue.
+and when the queue becomes empty, pop elements from stack and print.
+*/
