dcdf3ea13bb16694806933a1112b0d7fa8e897eb
Pranav <34658584+prkumar0603@users.noreply.github.com>
2021-05-24 13:54:40 +0530

    Multi-Threading Producer Consumer Problem

--- Algorithms/producerConsumer.java
+++ Algorithms/producerConsumer.java
@@ -4,8 +4,10 @@ import java.util.LinkedList;
 
 public class producerConsumer {
     public static void main(String[] args) throws InterruptedException{
+        // PC class has both produce and consume methods
         final PC pc = new PC();
 
+        // creating producer thread. Threads has inturrepted exception
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -17,6 +19,7 @@ public class producerConsumer {
             }
         });
 
+        // creating consumer thread. Threads has inturrepted exception
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -28,9 +31,11 @@ public class producerConsumer {
             }
         });
 
+        // for calling the run method of thread
         t1.start();
         t2.start();
-
+        
+        // join() method allows one Thread to waits until it completes its execution (child Thread terminates and join main thread)
         t1.join();
         t2.join();
     }
@@ -44,15 +49,17 @@ class PC{
         int value = 0;
 
         while(true){
+            // sync block so that only a producer or a consumer thread runs at a time
             synchronized (this){
                 if (list.size() == capacity)
                     wait();
 
+                // If the list is empty, the control passes to below the loop and it adds a value in the list.
                 System.out.println("Producer produced - "+value);
 
                 list.add(value++);
-                notify();
-                Thread.sleep(1000);
+                notify();           // notify once you have something in list, you can have the consumer thread consume it.
+                Thread.sleep(1000); // just for visualization
             }
         }
     }
@@ -60,14 +67,16 @@ class PC{
     public void consume() throws InterruptedException {
         while (true){
             synchronized (this){
+                
+                //If it is empty then we make the consumer thread give up the lock on PC and passes the control to producer thread for producing more jobs
                 if (list.size() == 0)
                     wait();
 
                 int val = list.removeFirst();
                 System.out.println("Consumer removed - "+val);
 
-                notify();
-                Thread.sleep(1000);
+                notify();           // if you have consumed something, you can have the producer produce something.
+                Thread.sleep(1000); // for visualization
             }
         }
     }
