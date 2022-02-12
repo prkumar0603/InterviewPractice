a3aee1bc55fee525b89eb9f1979135003228ee09
Pranav <34658584+prkumar0603@users.noreply.github.com>
2022-02-12 21:56:41 +0530

    Kadane Algorithm

--- Array/kadaneAlgorithm.java
+++ Array/kadaneAlgorithm.java
@@ -3,29 +3,31 @@ public class kadaneAlgorithm {
 
     public static void main(String[] args) {
        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+       //int arr[] = {-2, -3, 8, 1,-9, 1, 5, -3};
 
        int start = 0;   // for the start index of subarray
        int end = 0;     // for the end index of subarray
        int s =0;
 
-       int max_sum = Integer.MIN_VALUE;
+       int prev_sum = Integer.MIN_VALUE;
        int curr_sum = 0;
 
        for (int i=0; i<arr.length;i++){
            curr_sum += arr[i];
 
-           if (curr_sum < 0){
-               curr_sum = 0;
-               s = i+1;
-           }
-           else if (curr_sum > max_sum){
-               max_sum = curr_sum;
-               end =i;
-
-               start = s;
-           }
+           // check if new sum formed is greater than previous sum
+           if (curr_sum > prev_sum){
+                prev_sum = curr_sum;
+                start =s;
+                end = i;
+            }
+           // if new sum formed is < 0, make it zer0. and start will be from next of that element
+            if (curr_sum < 0){
+                curr_sum = 0;
+                s = i+1;
+            }
        }
-        System.out.println("start="+ start+"  end="+end);
-        System.out.println("Maximum sum = "+max_sum);
+        System.out.println("start = "+ start+" end = "+end);
+        System.out.println("Maximum sum = "+prev_sum);
     }
 }
