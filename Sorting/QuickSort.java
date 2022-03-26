fa1aa12754fff94e83c9f534ce7bbf4fa52255ea
Pranav <34658584+prkumar0603@users.noreply.github.com>
2022-03-26 23:52:43 +0530

    Quick Sort Recursive

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -2,6 +2,10 @@ package Sorting;
 
 import java.util.Arrays;
 
+/* Quick Sort - QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around the picked pivot. The key process in quickSort is partition(). Target of partitions is, given an array and an element x of array as pivot, put x at its correct position in sorted array and put all smaller elements (smaller than x) before x, and put all greater elements (greater than x) after x. 
+
+TIME COMPLEXITY - [n(logn)] in Best Case and Average Case. [n2] is Worst Case Time Complexity.
+*/
 public class QuickSort {
     public static void main(String[] args) {
         int[] arr = {10, 80, 50, 90, 40, 30, 70};
